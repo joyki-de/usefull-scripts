b9a2ff33c996d3bc9492e85ca92b168bac8b7746
joyki-de <joyki@gmx.de>
2018-06-19 20:08:03 +0200

    Bugfix

--- conan-exile/conan-script.sh
+++ conan-exile/conan-script.sh
@@ -1,4 +1,4 @@
-# Conan Scripter v1.0
+# Conan Scripter v1.1
 #
 # CC-BY Joyki
 #
@@ -8,21 +8,21 @@ starter="conan-start.sh"
 checker="conan-check.sh"
 if [ -f "$updater" ]
 then
-echo -e "[Updater] exists \xE2\x9C\x94"
+echo  "[Updater] exists"
 else
 cd ~
 touch conan-update.sh
 echo '#! /bin/bash
 echo "[Start Update Conan]"
 cd ~/Conan/
-./steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir conan/Serverfiles +login anonymous +app_update 443030 validate +quit
+./steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir Conan/Serverfiles +login anonymous +app_update 443030 validate +quit
 echo "[done]"' > $updater
 chmod +x conan-update.sh
-echo -e "[Updater] created \xE2\x9C\x94"
+echo  "[Updater] created"
 fi
 if [ -f "$starter" ]
 then
-echo -e "[Starter] exists \xE2\x9C\x94"
+echo  "[Starter] exists"
 else
 cd ~
 touch conan-start.sh
@@ -30,11 +30,11 @@ echo '#! /bin/bash
 cd ~/Conan/Serverfiles
 screen -dmS Conan /usr/src/wine-3.0/wine ConanSandboxServer.exe -log ' >$starter
 chmod +x conan-start.sh
-echo -e "[Starter] created \xE2\x9C\x94"
+echo  "[Starter] created"
 fi
 if [ -f "$checker" ]
 then
-echo -e "[Checker] exists \xE2\x9C\x94"
+echo  "[Checker] exists"
 else
 cd ~
 touch conan-check.sh
@@ -47,6 +47,6 @@ case "$(pidof Conan | wc -w)" in
 ;;
 esac' > $checker
 chmod +x conan-check.sh
-echo -e "[Checker] created \xE2\x9C\x94"
+echo  "[Checker] created"
 fi
 
