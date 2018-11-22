fdd45e6d7aecdc204f903e3102dee0f4ebfe6a2e
joyki-de <joyki@gmx.de>
2018-11-22 12:01:33 +0100

    added Auto-Installer, rename Script

--- Game-Server/Linux/ARK/ark-installer.sh
+++ Game-Server/Linux/ARK/ark-installer.sh
@@ -1,43 +1,50 @@
+#!/bin/bash
+#
 # Ark Installer v1.0
 #
 # CC-BY Joyki
 #
-if [ `echo -n $USER` != "root" ]
-then
 echo ""
-echo "[ERROR]: start as root"
+echo "ARK-Server Installer"
 echo ""
-exit 1
-fi
+PS3='Please choose: '
+options=("Install Requ.(root/sudo need!)" "Install Steam" "Create Scripts" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Install Requ.(root/sudo need!)")
+            echo "Install Requirements"
+# Safemode /on
+#sudo apt-get update 
+#sudo apt-get upgrade -y 
 #
-sleep 1
+#
+echo "Done"
 echo ""
-echo "[INST_1]: APT Update & Upgrade"
-apt-get update
-apt-get upgrade -y
-sleep 1
+
+            ;;
+        "Install Steam")
+            echo "Install Steam-Client"
+# Safemode /on
+# cd ~
+#mkdir Ark
+#cd Ark
+#curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+echo "Done"
 echo ""
-echo "[INST_2]: Install Packages"
-apt-get install lib32gcc1 curl screen ca-certificates libc6-dev build-essential -y
-sleep 1
+
+            ;;
+        "Create Scripts")
+            echo "Create Scripts"
+# Safemode /on
+
+
+echo "Done"
 echo ""
-echo "[INST_3]: Download Files"
-cd ~
-mkdir Ark
-cd Ark
-curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
-./steamcmd.sh +force_install_dir ~/Ark/Serverfiles +login anonymous +app_update 376030 validate +quit 
-sleep 1
-echo ""
-echo "[DONE]: Install is done"
-echo ""
-echo ""
-echo " To start the Server, type:"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo ""
-echo "screen"
-echo "cd ~/Ark/Serverfiles"
-echo "./ark-start.sh"
-echo ""
-echo " Have fun ! "
-echo ""
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
