e82b171e380bbf138178485b484f0e44a5e953ed
joyki-de <joyki@gmx.de>
2018-11-22 12:38:26 +0100

    Updates

--- Game-Server/Linux/ARK/ark-installer.sh
+++ Game-Server/Linux/ARK/ark-installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Ark Installer v1.0
+# Ark Installer v1.2
 #
 # CC-BY Joyki
 #
@@ -8,37 +8,62 @@ echo ""
 echo "ARK-Server Installer"
 echo ""
 PS3='Please choose: '
-options=("Install Requ.(root/sudo need!)" "Install Steam" "Create Scripts" "Quit")
+options=("Install Requ.(root/sudo need!)" "Install Server" "Create Scripts" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Install Requ.(root/sudo need!)")
-            echo "Install Requirements"
-# Safemode /on
-#sudo apt-get update 
-#sudo apt-get upgrade -y 
-#
-#
+            echo "Install Requirements, please wait..."
+sudo apt-get update 
+sudo apt-get upgrade -y 
+apt-get install lib32gcc1 curl screen ca-certificates libc6-dev build-essential -y
 echo "Done"
 echo ""
 
             ;;
-        "Install Steam")
-            echo "Install Steam-Client"
-# Safemode /on
-# cd ~
-#mkdir Ark
-#cd Ark
-#curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+        "Install Server")
+            echo "Install Steam-Client, please wait"
+cd ~
+mkdir Ark
+cd Ark
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+echo ""
+echo "Install Server"
+echo ""
+ ./steamcmd.sh +force_install_dir ~/Ark/Serverfiles +login anonymous +app_update 376030 validate +quit
 echo "Done"
 echo ""
-
             ;;
         "Create Scripts")
             echo "Create Scripts"
-# Safemode /on
-
-
+starter="ark-start.sh"
+updater="ark-update.sh"
+if [ -f "$updater" ]
+then
+echo  "[Updater] exists"
+else
+cd ~
+touch ark-update.sh
+echo '#! /bin/bash
+echo "[Start Update Ark]"
+cd ~/Ark/
+./steamcmd.sh +force_install_dir ~/Ark/Serverfiles +login anonymous +app_update 376030 validate +quit
+echo "[done]"' > $updater
+chmod +x ark-update.sh
+echo  "[Updater] created"
+fi
+if [ -f "$starter" ]
+then
+echo  "[Starter] exists"
+else
+cd ~
+touch ark-start.sh
+echo '#! /bin/bash
+cd ~/Ark/Serverfiles
+screen -dmS Ark /ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=SERVERNAME -server -log' >$starter
+chmod +x ark-start.sh
+echo  "[Starter] created"
+fi
 echo "Done"
 echo ""
             ;;
