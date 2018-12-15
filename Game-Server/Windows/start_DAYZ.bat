6c0441bbdd326403f7f26001cbce969f639b818c
Joyki@gmx.de <joyki@gmx.de>
2018-12-15 14:17:25 +0100

    added DayZ

---
+++ Game-Server/Windows/start_DAYZ.bat
@@ -0,0 +1,6 @@
+@echo off
+
+TITLE DAYZ-SERVER
+
+start ""/NORMAL "C:\STEAM\steamcmd\dayz\DayZServer_x64.exe" "-instanceid=1" "-config=serverDZ.cfg"
+exit
---
+++ Game-Server/Windows/update_DAYZ.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE UPDATE DAYZ
+C:\STEAM\steamcmd\steamcmd.exe +login NAME! +force_install_dir ./DAYZ/ +app_update 223350 validate +quit
+exit
