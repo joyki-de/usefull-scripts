78cebe4756ed718376a33b003cb49af1b527353c
Joyki <Joyki@gmx.de>
2019-10-04 18:23:50 +0200

    added request of tbl & Wildstar Client Path

--- Game-Server/Linux/NexusForever/install-server.sh
+++ Game-Server/Linux/NexusForever/install-server.sh
@@ -1,3 +1,20 @@
+while true
+do
+echo ""
+echo -n "Enter Path to tbl Files (e.g. /home/files/) "
+read -e TBFPATH
+echo ""
+echo -n "Enter Path to Wildstar-Client (e.g. /home/files/WildstarClient/) :" 
+read -e WILDSTAR
+echo ""
+echo -n "tbl-Files on ${TBFPATH} and Wildstar-Client on ${WILDSTAR} correct?"
+echo -n "Don't forget the / on the End of the Patch !"
+               case $CONFIRM in
+                        y|Y|YES|yes|Yes|j|J)
+                                break ;;
+                        *) echo "Please enter Path again"
+                esac
+done
 mkdir ${HOME}/git
 cd ${HOME}/git
 git clone https://github.com/NexusForever/NexusForever.git
@@ -14,14 +31,14 @@ done
 
 cd NexusForever.WorldServer/
 sed -i -e "s|\(.*MapPath\": \"\)\(.*\)|\1$PWD/\2|" WorldServer.json
-cp -a /path/to/tbl/files .
+cp -a ${TBFPATH} .
 mkdir -pv bin/Debug/${framework_version}/
 ln -s ${PWD}/tbl/ ${PWD}/bin/Debug/${framework_version}/
 cd ..
 
 export TERM=xterm
 cd NexusForever.MapGenerator/
-dotnet run /path/to/wildstar_directory/WildStar/Patch/
+dotnet run ${WILDSTAR}Patch/
 cp -a output ../NexusForever.WorldServer/map
 cd ..
 
