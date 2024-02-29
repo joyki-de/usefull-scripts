d85c13c5a0ebec2d1880aff2f020a8689268aaf6
Joyki <Joyki@gmx.de>
2024-02-29 22:15:12 +0100

    garbage collection

--- Arduino/OLED_PV.php
+++
@@ -1,7 +0,0 @@
-Power: <?php
-$pdo = new PDO('mysql:host=localhost;dbname=goodwe', 'raspi', 'raspi');
-
-$sql = "SELECT * FROM pv ORDER BY id DESC LIMIT 1";
-foreach ($pdo->query($sql) as $row)
-echo $row['power'];
-?>
